ec3c9b291c49e0362a287ea1ab0f586e559d0e89
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2023-03-17 09:56:55 -0400

    refactor: `SysInfo` use tokio::fs

--- src/api/routers/admin.rs
+++ src/api/routers/admin.rs
@@ -35,18 +35,18 @@ struct SysInfo {
 }
 
 impl SysInfo {
-    fn new(start_time: SystemTime) -> Self {
+    async fn new(start_time: SystemTime) -> Self {
         // When running in docker, pid should always be 1
         let pid = std::process::id();
 
-        let memory = std::fs::read_to_string(format!("/proc/{pid}/statm"))
+        let memory = tokio::fs::read_to_string(format!("/proc/{pid}/statm")).await
             .unwrap_or_default()
             .split(' ')
             .take(2)
             .map(|i| i.parse::<usize>().unwrap_or_default() * 4096)
             .collect::<Vec<_>>();
 
-        let uptime = std::fs::read_to_string("/proc/uptime")
+        let uptime = tokio::fs::read_to_string("/proc/uptime").await
             .unwrap_or_default()
             .split('.')
             .take(1)
@@ -293,11 +293,10 @@ impl AdminRouter {
     }
 
     /// Get server info, uptime, app uptime, virt mem, and rss memory
-    #[allow(clippy::unused_async)]
     async fn memory_get(
         State(state): State<ApplicationState>,
     ) -> Result<Outgoing<oj::AdminMemory>, ApiError> {
-        let sysinfo = SysInfo::new(state.start_time);
+        let sysinfo = SysInfo::new(state.start_time).await;
         Ok((
             StatusCode::OK,
             oj::OutgoingJson::new(oj::AdminMemory {
