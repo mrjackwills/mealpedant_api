14f54c03e4b33064918543d8b0f73de9bc0302d9
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2023-10-06 21:35:41 +0100

    fix: banned domains postgres init
    delete all domains, then copy into, domains list also updated

--- docker/init/postgres_init.sh
+++ docker/init/postgres_init.sh
@@ -2,7 +2,7 @@
 set -e
 
 create_mealpedant_user_and_db() {
-	echo "create_mealpedant_user_and_db"
+	printf "\ncreate_mealpedant_user_and_db\n\n"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
 CREATE ROLE $DB_NAME WITH LOGIN PASSWORD '$DB_PASSWORD';
 CREATE DATABASE $DB_NAME;
@@ -10,7 +10,7 @@ EOSQL
 }
 
 restore_pg_dump() {
-	echo "restore_pg_dump"
+	printf "\nrestore_pg_dump\n\n"
 	# --single-transaction?
 	pg_restore -U "$POSTGRES_USER" -O --exit-on-error --single-transaction -d "$DB_NAME" -v /docker-entrypoint-initdb.d/pg_dump.tar
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB_NAME" <<-EOSQL
@@ -20,11 +20,11 @@ EOSQL
 }
 
 update_banned_domains () {
-	echo "update_banned_domains"
+	printf "\nupdate_banned_domains\n\n"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB_NAME" <<-EOSQL
-COPY banned_email_domain (domain) FROM '/docker-entrypoint-initdb.d/banned_domains.txt';
 DELETE FROM banned_email_domain;
-GRANT USAGE, SELECT ON SEQUENCE banned_email_domain_banned_email_domain_id_seq TO $DB_NAME;
+COPY banned_email_domain (domain) FROM '/docker-entrypoint-initdb.d/banned_domains.txt';
+GRANT USAGE, SELECT ON SEQUENCE banned_domain_banned_domain_id_seq TO $DB_NAME;
 GRANT ALL ON banned_email_domain TO $DB_NAME;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO $DB_NAME;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_NAME;
