4ad24be544e28677623e3bc436d32f1297116948
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-01-30 19:19:01 +0000

    fix argon cfg debug

--- src/argon.rs
+++ src/argon.rs
@@ -9,7 +9,7 @@ use tracing::error;
 use crate::api_error::ApiError;
 
 #[allow(clippy::unwrap_used)]
-#[cfg(not(release))]
+#[cfg(debug_assertions)]
 static PARAMS: Lazy<Params> = Lazy::new(|| {
     ParamsBuilder::new()
         .m_cost(4096)
@@ -20,8 +20,8 @@ static PARAMS: Lazy<Params> = Lazy::new(|| {
 });
 
 /// This takes 19 seconds when testing, hence the above `not-release` version
-#[cfg(release)]
 #[allow(clippy::unwrap_used)]
+#[cfg(not(debug_assertions))]
 static PARAMS: Lazy<Params> = Lazy::new(|| {
     ParamsBuilder::new()
         .m_cost(24 * 1024)
