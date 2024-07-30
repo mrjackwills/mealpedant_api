40c41d9f9fc7279ffe928aa203cae4b59ce9d12c
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-07-30 15:12:39 +0100

    refactor: replace OnceCell with std::sync::LazyLock

--- src/api/deserializer.rs
+++ src/api/deserializer.rs
@@ -1,10 +1,9 @@
-use once_cell::sync::Lazy;
 use regex::Regex;
 use serde::{
     de::{self, IntoDeserializer},
     Deserialize, Deserializer,
 };
-use std::net::IpAddr;
+use std::{net::IpAddr, sync::LazyLock};
 use time::{Date, Month};
 use uuid::Uuid;
 
@@ -18,7 +17,7 @@ use super::{ij::LimitKey, incoming_json::ij};
 pub struct IncomingDeserializer;
 
 #[allow(clippy::unwrap_used)]
-static REGEX_EMAIL: Lazy<Regex> = Lazy::new(|| {
+static REGEX_EMAIL: LazyLock<Regex> = LazyLock::new(|| {
     Regex::new(r#"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#).unwrap()
 });
 
